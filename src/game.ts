ab497a22b16378cfa2ca721b84830ec7441a1e00
NosuRed <behrensphilipp1@gmail.com>
2023-07-04 06:03:16 +0200

    Change the Ball class lul

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import {Ball} from './Ball';
 // rules used: https://www.billardpro.de/pool-billard-regeln#achtball
 
 export default class Billiard extends Phaser.Scene {
@@ -228,40 +229,3 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
-
-
-class Ball extends Phaser.Physics.Arcade.Sprite {
-    private id: number = 0;
-
-    constructor(config) {
-        super(config.scene, config.x, config.y, config.texture);
-        config.scene.add.existing(this);
-        this.id = config.id;
-
-
-        // Add the ball to the scene and enable physics
-        config.scene.physics.add.existing(this);
-
-        // Set the bounce property for the ball
-        this.setBounce(1,1);
-        this.setCollideWorldBounds(true);
-        this.body.setMass(1);
-        this.setDrag(75, 75);
-
-    }
-
-    update() {
-        if (Math.abs(this.body.velocity.x) <= 2) {
-            this.body.velocity.x = 0;
-        }
-        if (Math.abs(this.body.velocity.y) <= 2) {
-            this.body.velocity.y = 0;
-        }
-    }
-
-    getID() {
-        return this.id;
-    }
-
-
-}
